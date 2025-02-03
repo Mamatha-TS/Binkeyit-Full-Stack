83be7e63aadf713581a7114cbde7d29c9f6ba1b5
mamatha ts <vvce22cse0139@vvce.ac.in>
2025-02-03 22:26:42 +0530

    changes made

--- server/config/sendEmail.js
+++ server/config/sendEmail.js
@@ -11,7 +11,7 @@ const resend = new Resend(process.env.RESEND_API);
 const sendEmail = async({sendTo, subject, html })=>{
     try {
         const { data, error } = await resend.emails.send({
-            from: 'Binkeyit <noreply@blinkit.reply.com>',
+            from: 'Binkeyit <noreply@binkeyit.com>',
             to: sendTo,
             subject: subject,
             html: html,
